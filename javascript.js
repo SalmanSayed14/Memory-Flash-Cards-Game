ef7511d55fc6f42ed1f8c5a80a0166fdb8f848b9
SalmanSayed14 <SSayed1410@gmail.com>
2024-05-08 22:01:40 +0300

    select the first card and second

--- javascript.js
+++ javascript.js
@@ -1,6 +1,14 @@
 const allCards = document.querySelectorAll('.card')
+let firstCard, secondCard
 allCards.forEach((card) => card.addEventListener('click', handleCardClicked))
 
 function handleCardClicked() {
   this.classList.add('flip')
+  if (!isFlippedCard) {
+    isFlippedCard = true
+    firstCard = this
+  } else {
+    isFlippedCard = false
+    secondCard = this
+  }
 }
