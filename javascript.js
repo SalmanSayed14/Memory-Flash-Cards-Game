07fc8badae97e5dcfd4cd0ffcc7b96175a2c2e90
SalmanSayed14 <SSayed1410@gmail.com>
2024-05-09 00:19:46 +0300

    add command to activate the mode buttom

--- javascript.js
+++ javascript.js
@@ -4,6 +4,21 @@ let isFlippedCard = false
 let isInProcess = true
 let errors = 0
 
+let body = document.querySelector('body')
+let screenmod = true
+const screenmode = document.querySelector('#screenMode')
+screenmode.addEventListener('click', () => {
+  if (screenmod === true) {
+    body.style.backgroundImage =
+      'url(https://as2.ftcdn.net/v2/jpg/02/18/72/77/1000_F_218727724_ogFtvbGlr92YNwnCMmthutcwcWJ2Lfxz.jpg)'
+    screenmod = false
+  } else if (screenmod === false) {
+    body.style.backgroundImage =
+      'url(https://media.istockphoto.com/id/1401568968/vector/blue-matrix-background-abstract-binary-code-wallpaper-template-for-hackathon-and-other.jpg?s=612x612&w=0&k=20&c=7WQvXlYcGvriTqRg-bQENTqDTISNnmOB5pSeprNqGBQ=)'
+    screenmod = true
+  }
+})
+
 allCards.forEach((card) => card.addEventListener('click', handleCardClicked))
 
 allCards.forEach((card) => {
