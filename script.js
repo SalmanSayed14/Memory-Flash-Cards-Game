34b983d4c17dcf6cad1c3839106517170266b435
SalmanSayed14 <SSayed1410@gmail.com>
2024-05-18 13:43:59 +0300

    abb function 4 seconds

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+window.onload = function () {
+  setTimeout(function () {
+    const images = document.querySelectorAll('.image')
+    images.forEach(function (image) {
+      image.style.display = 'none'
+    })
+  }, 4000) // 4000 milliseconds = 4 seconds
+}
