0eeedd098443fc04c883e24d9f386be0800fd52d
SalmanSayed14 <SSayed1410@gmail.com>
2024-05-19 07:04:03 +0300

    add thirdClick

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const allBtns = document.querySelectorAll('.btn')
 const allBtns1 = document.querySelectorAll('.btn1')
 const allImages = document.querySelectorAll('.image')
+const allImages1 = document.querySelectorAll('.image1')
 const loseMeg = document.querySelector('.loseMge')
 const winMeg = document.querySelector('.winMge')
 let wrongs = 0
@@ -19,10 +20,22 @@ window.onload = function () {
     loseMeg.style.display = 'block'
   }, 60000)
 }
+//// for level two
+window.onload = function () {
+  setTimeout(function () {
+    allImages1.forEach(function (image1) {
+      image1.style.display = 'none'
+    })
+  }, 4000)
+  setTimeout(function () {
+    loseMeg.style.display = 'block'
+  }, 60000)
+}
 
 /// Determine the first and second click
 let firstClicked = null
 let secondClicked = null
+let thirdClicked = null
 let hasClicked = false
 
 // Add event listener to each button
@@ -31,10 +44,10 @@ allBtns.forEach((btn) => {
 })
 
 allBtns1.forEach((btn1) => {
-  btn1.addEventListener('click', handleClick)
+  btn1.addEventListener('click', handleClick1)
 })
 
-/// rando images for level two
+/// random images for level two
 allBtns1.forEach((btn1) => {
   let random = Math.floor(Math.random() * 10)
   btn1.style.order = random
@@ -70,6 +83,57 @@ function handleClick(event) {
       wrongs += 1
       firstClicked = null
       secondClicked = null
+      hasClicked = false
+      if (wrongs === 3) {
+        loseMeg.style.display = 'block'
+      }
+    }
+  }
+}
+
+function handleClick1(event) {
+  if (!firstClicked) {
+    firstClicked = this
+  } else if (!secondClicked) {
+    secondClicked = this
+  } else {
+    thirdClicked = this
+    hasClicked = true
+  }
+
+  // If this is the second click, check if the images match
+  if (firstClicked && secondClicked && thirdClicked) {
+    const firstImageData = firstClicked.getAttribute('data-image')
+    const secondImageData = secondClicked.getAttribute('data-image')
+    const thirdImageData = thirdClicked.getAttribute('data-image')
+    const firstClickedImage = firstClicked.querySelector('.image1')
+    const secondClickedImage = secondClicked.querySelector('.image1')
+    const thirdClickedImage = thirdClicked.querySelector('.image1')
+    if (
+      secondImageData === firstImageData &&
+      thirdImageData === firstImageData
+    ) {
+      alert('Great!')
+      firstClicked = null
+      secondClicked = null
+      thirdClicked = null
+      hasClicked = false
+      firstClickedImage.style.display = 'block'
+      secondClickedImage.style.display = 'block'
+      thirdClickedImage.style.display = 'block'
+      corrects += 1
+      if (corrects === 5) {
+        winMeg.style.display = 'block'
+      }
+    } else if (
+      secondImageData != firstImageData &&
+      firstImageData != thirdClickedImage
+    ) {
+      alert('wrong!')
+      wrongs += 1
+      firstClicked = null
+      secondClicked = null
+      thirdClicked = null
       hasClicked = false
       if (wrongs === 3) {
         loseMeg.style.display = 'block'
